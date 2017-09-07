d44516a5cf46a172b2ba9fe9635e930486dd2878
xiaohui <826581113@qq.com>
2017-09-07 19:32:21 +0800

    新增学习模块

---
+++ springcloudknowledge/src/main/java/semaphores/SemaphoreTest.java
@@ -0,0 +1,35 @@
+package semaphores;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by xh826 on 2017/9/7.
+ */
+public class SemaphoreTest {
+    public static void main(String[] args) {
+        // 线程池
+        ExecutorService exec = Executors.newCachedThreadPool();
+        // 只能5个线程同时访问
+        final Semaphore semp = new Semaphore(5);
+        for (int index = 0; index < 20; index++) {
+            final int NO = index;
+            Runnable run = new Runnable() {
+                public void run() {
+                    try {
+                        semp.acquire();
+                        System.out.println("Accessing: " + NO);
+                        Thread.sleep((long) (Math.random() * 10000));
+                        semp.release();
+                        System.out.println("-----------------" + semp.availablePermits());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            exec.execute(run);
+        }
+        exec.shutdown();
+    }
+}
---
+++ springcloudknowledge/src/main/java/semaphores/Test.java
@@ -0,0 +1,14 @@
+package semaphores;
+
+import java.util.Date;
+
+/**
+ * Created by xh826 on 2017/9/7.
+ */
+public class Test {
+    public static void main(String[] args) {
+        long a = Long.valueOf("1479174235000");
+        Date date = new Date();
+        System.out.printf(date.toString());
+    }
+}
